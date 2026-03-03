83dcdcc12c938ebd69bee2a554c8d72f97f075ad
Vladimir Oscanoa <245904981+vladimir-devlad@users.noreply.github.com>
2026-03-03 17:56:45 -0500

    feature: Update sync

--- src/api/reportesApi.js
+++ src/api/reportesApi.js
@@ -4,3 +4,4 @@ export const getReportes = (params) =>
   axiosClient.get("/reportes/", { params });
 
 export const getReporteById = (id) => axiosClient.get(`/reportes/${id}`);
+export const getLastSync = () => axiosClient.get("/sync/last");
